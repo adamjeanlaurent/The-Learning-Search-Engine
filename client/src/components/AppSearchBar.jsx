877deeb119b8befe8a7167df8052136634078f72
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-04-16 21:02:41 -0400

    cleanup of searchbar

--- client/src/components/AppSearchBar.jsx
+++ client/src/components/AppSearchBar.jsx
@@ -5,6 +5,17 @@ import "../css/searchBar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function AppSearchBar(props) {
+  function handleSearchButtonClick() {
+    let sb = document.querySelector(".input-group-text");
+    sb.style.backgroundColor = "white";
+
+    setTimeout(() => {
+      sb.style.backgroundColor = "";
+    }, 100);
+
+    // call function from props to populate and stuff in here too
+  }
+
   return (
     <div>
       <ScriptTag
@@ -13,14 +24,15 @@ export default function AppSearchBar(props) {
       />
       <Container>
         <div className="md-form mt-0 searchBar">
-          <div class="input-group-text">
-            <i class="fas fa-search"></i>
+          <div className="input-group-text" onClick={handleSearchButtonClick}>
+            <i className="fas fa-search"></i>
           </div>
           <input
             className="form-control"
             type="text"
-            placeholder="Search"
+            placeholder="I Want To Learn About ...."
             aria-label="Search"
+            autoComplete="false"
           />
         </div>
       </Container>
