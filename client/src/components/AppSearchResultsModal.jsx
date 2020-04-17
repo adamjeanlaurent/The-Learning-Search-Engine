a49cd53912e7dae4e45bb1d64d4df77ceaa47f45
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-04-16 21:18:21 -0400

    framework for future work with dynamic results

---
+++ client/src/components/AppSearchResultsModal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container } from "reactstrap";
+
+/*
+    We are going to have a dynamic number of rows, each rows has 3 columns
+    going to have to think about how we want to rows to collapse in the future
+*/
+
+export default function AppSearchResultsModal(props) {
+  return (
+    <div>
+      <Container></Container>
+      <h1>hello</h1>
+    </div>
+  );
+}
